d437d53e51d95ae933beff6ee9651d64898b104e
Cello <marcelloezra61@gmail.com>
2024-11-11 15:45:52 +0700

    minggu 12

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PegawaiDBController;
 use App\Http\Controllers\BlogController;
 
 /*
@@ -32,6 +33,10 @@ Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/blog2', [DosenController::class, 'blog']);
 Route::get('/biodata', [DosenController::class, 'biodata']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+//Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
